28ee9429b61a4bcac920306008fca616eb673713
MohammedGhazaly <warendragon67@gmail.com>
2023-10-27 02:20:07 +0200

    Finished movie details service

--- lib/features/movie_details/view/widgets/movie_details_hero.dart
+++ lib/features/movie_details/view/widgets/movie_details_hero.dart
@@ -8,23 +8,11 @@ class MovieDetailsHero extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Image.asset(
-          "assets/images/background.png",
-          width: double.infinity,
-          height: 200.h,
-          fit: BoxFit.fill,
-        ),
-        Positioned.fill(
-          child: Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              "assets/icons/play_icon.png",
-            ),
-          ),
-        ),
-      ],
+    return Image.asset(
+      "assets/images/background.png",
+      width: double.infinity,
+      height: 200.h,
+      fit: BoxFit.fill,
     );
   }
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:movies_app/core/constants/api_constants.dart';
+import 'package:movies_app/models/movie_details_model/movie_details_model.dart';
 import 'package:movies_app/models/movie_model/movie_response_model.dart';
 import "package:http/http.dart" as http;
 
@@ -15,6 +16,24 @@ class ApiService {
       Map<String, dynamic> jsonData = jsonDecode(response.body);
 
       var responseData = MoveiResponse.fromJson(jsonData);
+      return responseData;
+    } on Exception catch (e) {
+      throw e;
+    }
+  }
+
+  static Future<MovieDetails> movieDetails(String movieId) async {
+    try {
+      Uri url = Uri.https(
+        ApiConstants.baseUrl,
+        ApiConstants.movieEndPoint,
+        {"api_key": ApiConstants.apiKey, "movie_id": movieId},
+      );
+      var response = await http.get(url);
+
+      Map<String, dynamic> jsonData = jsonDecode(response.body);
+
+      var responseData = MovieDetails.fromJson(jsonData);
       return responseData;
     } on Exception catch (e) {
       throw e;
